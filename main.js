de381549881e1b152ab2181658631b0c2b529e4a
Joel Crouch <crouchj@cecs.pdx.edu>
2021-11-03 05:24:01 +0000

    output for clock, + function to tie it all (main)

--- main.js
+++ main.js
@@ -16,10 +16,39 @@ function timer(userinput){
     const seconds = Math.floor(difference/(1000)%60);
     return {difference, minutes, seconds};
 }
-//function that updates the clock 
+//function that makes & updates the clock 
+function newclock(id, userinput){
+    const count = document.getElementById(id);
+    const holdminutes= count.querySelector(".minutes");//these goto the span(long term storage)
+    const holdseconds = count.querySelector(".seconds");
+    //insdie of the clock function you update it!!!
+    function update(){
+        const out = timer(userinput);
+        holdminutes.innerHTML=out.minutes;
+        holdseconds.innerHTML=("0" + out.seconds);
+        if(out.difference <=0){
+            clearInterval(between);
+        }
+    }
+    update();
+    const between =setInterval(newclock, 1000);  //do that every second
+}
+function alltogether(worktime, restime){
+    for(var i=0; i<2; i++){
+    //the work 3xt
+        timer(worktime);
+        newclock(Pomodortimer, worktime);
+        timer(restime);
+        newclock(Pomodortimer, restime);
+    }
+    timer(worktime);
+    newclock(Pomodortimer, worktime);
+    timer(restime);
+    newclock(Pomodortimer, restime);
+    //after the 3rd work doa rest with 10 extra Minutes
+}
 //Pause?
-//Stop
-
+//Stop[p;]
 
 
 
