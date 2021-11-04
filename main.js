fbc1d8cebc013c6f46a9ec37981fc492e06d150c
Joel Crouch <crouchj@cecs.pdx.edu>
2021-11-04 16:24:55 +0000

    made the timer work

--- main.js
+++ main.js
@@ -3,49 +3,17 @@
 //function that takes in time from user
 //const inputtime=userinput;
 //need a button to send minutes to function
-function timer(userinput){
-    const now = Date.parse(new Date())
-    const end=new Date(now + inputtime*60*1000);
-    const difference = end-now;
-    const hours = Math.floor(difference/(1000*60*60)%24);
-        if(hours >0 ){
-            alert("Please choose a time less than 60 minutes.");
-            return;
-        }
-    const minutes = Math.floor(difference/(1000/60)%60);
-    const seconds = Math.floor(difference/(1000)%60);
-    return {difference, minutes, seconds};
-}
-//function that makes & updates the clock 
-function newclock(id, userinput){
-    const count = document.getElementById(id);
-    const holdminutes= count.querySelector(".minutes");//these goto the span(long term storage)
-    const holdseconds = count.querySelector(".seconds");
-    //insdie of the clock function you update it!!!
-    function update(){
-        const out = timer(userinput);
-        holdminutes.innerHTML=out.minutes;
-        holdseconds.innerHTML=("0" + out.seconds);
-        if(out.difference <=0){
-            clearInterval(between);
-        }
-    }
-    update();
-    const between =setInterval(newclock, 1000);  //do that every second
-}
-function alltogether(worktime, restime){
-    for(var i=0; i<2; i++){
-    //the work 3xt
-        timer(worktime);
-        newclock(Pomodortimer, worktime);
-        timer(restime);
-        newclock(Pomodortimer, restime);
-    }
-    timer(worktime);
-    newclock(Pomodortimer, worktime);
-    timer(restime);
-    newclock(Pomodortimer, restime);
-    //after the 3rd work doa rest with 10 extra Minutes
+function timer(){
+    var clockupdate=document.getElementById("clock");
+    var total= (document.getElementById('work').value * 60);//totsl seconds
+    minutes=Math.floor(total/(60));//whole minutes
+    seconds =total%minutes;
+    //send to the html output "clock"
+    var output=minutes.toString() +":"+seconds;
+    clockupdate.innerHTML=output;
+    total--;
+    setInterval(timer, 1000);
+    //alert(total);
 }
 //Pause?
 //Stop[p;]
